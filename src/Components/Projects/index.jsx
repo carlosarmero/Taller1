d8920eaab2b7013218ee35ca81d26e2795e2548d
horus <carloch1234@gmail.com>
2024-03-02 11:44:40 -0500

    ejercicio 3 ok

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -7,11 +7,11 @@ const CardProject = () => {
     <><p className='mt-2 mb-2'>
       A continuacion presento aplicaciones que he desarrollado:
     </p>
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font bg-cyan-100">
         <div className="container px-5 py-24 mx-auto flex flex-wrap">
-          <div className="flex flex-wrap -m-4">
-            <div className="p-4 lg:w-1/2 md:w-full">
-              <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+          <div className="flex flex-wrap -m-4 ">
+            <div className="p-4 lg:w-1/2 md:w-full ">
+              <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col bg-blue-100">
                 <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 flex-shrink-0">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-8 h-8" viewBox="0 0 24 24">
                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
@@ -29,7 +29,7 @@ const CardProject = () => {
               </div>
             </div>
             <div className="p-4 lg:w-1/2 md:w-full">
-              <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+              <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col bg-blue-100">
                 <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 flex-shrink-0">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
@@ -41,7 +41,7 @@ const CardProject = () => {
                   <p className="leading-relaxed text-base">El usuario podrá comprobar el inventario y actualizarlo, procesar ventas, registrar pedidos. Este 
     software está destinado a mejorar el uso del de tiempo para los empleados al realizar las tareas mencionadas
     anteriormente.</p>
-                  <a className="mt-3 text-blue-500 inline-flex items-center">Learn More
+                  <a className="mt-3 text-blue-500 inline-flex items-center" href='https://github.com/carlosarmero/adsi'>Enlace al codigo en GitHub
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
--- src/Pages/Sobre/sobre.jsx
+++ src/Pages/Sobre/sobre.jsx
@@ -1,17 +1,17 @@
 export default function Sobre() {
   const data = [
-    { name: 'Java', country: 'Junior', bio: 'Se crea aplicacion de escritorio con lenguaje Java' },
-    { name: 'Python', country: 'Junior', bio: 'Se crea pagina web basada en patrón CRUD' },
-    { name: 'Javascript', country: 'Junior', bio: 'Se hacen diferentes pruebas como esta pagina basada en React' },
-    { name: 'SQL', country: 'Junior', bio: 'Se manejan los conceptos basicos de bases de datos relacionales' },
-    { name: 'Git', country: 'Junior', bio: 'Uso del conntrol de versiones tanto local como en GitHub' },
-    { name: 'HTML Y CSS', country: 'Junior', bio: 'Practicas en diferentes proyectos personales' },
+    { name: 'Java', nivel: 'Junior', exp: 'Se crea aplicacion de escritorio con lenguaje Java' },
+    { name: 'Python', nivel: 'Junior', exp: 'Se crea pagina web basada en patrón CRUD' },
+    { name: 'Javascript', nivel: 'Junior', exp: 'Se hacen diferentes pruebas como esta pagina basada en React' },
+    { name: 'SQL', nivel: 'Junior', exp: 'Se manejan los conceptos basicos de bases de datos relacionales' },
+    { name: 'Git', nivel: 'Junior', exp: 'Uso del conntrol de versiones tanto local como en GitHub' },
+    { name: 'HTML Y CSS', nivel: 'Junior', exp: 'Practicas en diferentes proyectos personales' },
   ];
 
   return (
-    <table className="table-auto w-full">
+    <table className="table-auto w-full bg-rose-50">
       <thead>
-        <tr className='bg-violet-200 '>
+        <tr className='bg-orange-400 '>
           <th className="px-4 py-2">Tecnología</th>
           <th className="px-4 py-2">Nivel de experiencia</th>
           <th className="px-4 py-2">Descripción experiencia</th>
@@ -21,8 +21,8 @@ export default function Sobre() {
         {data.map((row) => (
           <tr key={row.id}>
             <td className="border px-4 py-2">{row.name}</td>
-            <td className="border px-4 py-2">{row.country}</td>
-            <td className="border px-4 py-2">{row.bio}</td>
+            <td className="border px-4 py-2">{row.nivel}</td>
+            <td className="border px-4 py-2">{row.exp}</td>
           </tr>
         ))}
       </tbody>
