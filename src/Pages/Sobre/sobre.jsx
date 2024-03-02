5696b5e20de0deb9b4b4857d42e077423ca99262
horus <carloch1234@gmail.com>
2024-03-02 11:07:03 -0500

    ejercicio 3

--- src/Pages/Sobre/sobre.jsx
+++ src/Pages/Sobre/sobre.jsx
@@ -1,7 +1,31 @@
 export default function Sobre() {
-    return (
-      <h1 className="bg-lime-400">
-        Sobre mi..
-      </h1>
-    )
-  }
+  const data = [
+    { name: 'Java', country: 'Junior', bio: 'Se crea aplicacion de escritorio con lenguaje Java' },
+    { name: 'Python', country: 'Junior', bio: 'Se crea pagina web basada en patrón CRUD' },
+    { name: 'Javascript', country: 'Junior', bio: 'Se hacen diferentes pruebas como esta pagina basada en React' },
+    { name: 'SQL', country: 'Junior', bio: 'Se manejan los conceptos basicos de bases de datos relacionales' },
+    { name: 'Git', country: 'Junior', bio: 'Uso del conntrol de versiones tanto local como en GitHub' },
+    { name: 'HTML Y CSS', country: 'Junior', bio: 'Practicas en diferentes proyectos personales' },
+  ];
+
+  return (
+    <table className="table-auto w-full">
+      <thead>
+        <tr className='bg-violet-200 '>
+          <th className="px-4 py-2">Tecnología</th>
+          <th className="px-4 py-2">Nivel de experiencia</th>
+          <th className="px-4 py-2">Descripción experiencia</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row) => (
+          <tr key={row.id}>
+            <td className="border px-4 py-2">{row.name}</td>
+            <td className="border px-4 py-2">{row.country}</td>
+            <td className="border px-4 py-2">{row.bio}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
